8829a42d6126d995ae52e31bd8ba7747d245279b
Guilherme Janczak <guilherme.janczak@yandex.com>
2022-04-08 22:41:15 +0000

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$ver = $env:ChocolateyPackageVersion
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = "https://github.com/guijan/dictpw/releases/download/v${ver}/setup-dictpw-${ver}-mingw32.exe"
+  url64bit      = "https://github.com/guijan/dictpw/releases/download/v${ver}/setup-dictpw-${ver}-mingw64.exe"
+  softwareName  = 'dictpw'
+
+  checksum      = 'cf4a0af1de855053dc5ef389b2d94bed62c68e9340e7ba52ba374a5f7c21373f'
+  checksumType  = 'sha256'
+  checksum64    = '78224a2cefc00d7440410ad507fa2625e046a39fc2d1837068c3a1efb781096b'
+  checksumType64= 'sha256'
+
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+$installLocation = Get-AppInstallLocation $packageArgs.softwareName
+if (!$installLocation)  {  Write-Warning "Can't find $PackageName install location"; return }
+Install-Binfile -Name $packageName -Path "$installLocation/bin/dictpw.exe"
+Write-Host "$packageName installed to '$installLocation'"
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,1 @@
+﻿Uninstall-BinFile -name $packageName
